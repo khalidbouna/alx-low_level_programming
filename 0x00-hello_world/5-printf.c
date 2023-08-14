188900f0d43e9183820c9d28fbf9d816a6c8a0ca
khalidbouna <bouna.ck@gmail.com>
2023-08-14 16:27:04 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
