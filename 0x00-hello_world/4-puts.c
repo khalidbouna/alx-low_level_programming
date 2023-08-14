e95501d73544b7b859844f57afcd186aed24e5c7
khalidbouna <bouna.ck@gmail.com>
2023-08-14 16:21:00 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * main - this is the main function
  *
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
