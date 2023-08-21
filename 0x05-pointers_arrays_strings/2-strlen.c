6723864e8f23e3ed2fd007166b7f36da0c7a5767
khalidbouna <bouna.ck@gmail.com>
2023-08-21 14:37:36 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - retutns the lenght of a string
+ *
+ * @s: string parameter input
+ *
+ * return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+  int counter;
+
+  for ( counter = 0; *0 != '\0'; ++s)
+    ++counter;
+
+  return (counter)
+    }
