f96b959ede9c0c084338805a17cd09b869966d85
khalidbouna <bouna.ck@gmail.com>
2024-02-07 14:18:25 -0800

    ma

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -11,15 +11,9 @@ unsigned long int hash_djb2(const unsigned char *str)
     unsigned long int hash;
     int c;
 
-    /* Initialize the hash to a constant value */
     hash = 5381;
-
-    /* Iterate through each character of the string */
     while ((c = *str++))
     {
-        /* Update the hash value using the djb2 algorithm:
-         * hash * 33 + c
-         */
         hash = ((hash << 5) + hash) + c;
     }
     return (hash);
