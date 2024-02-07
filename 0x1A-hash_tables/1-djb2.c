5c3723a65a04f3fb0be7a466e4e6258ff15ec4bc
khalidbouna <bouna.ck@gmail.com>
2024-02-07 14:13:19 -0800

    ma

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
